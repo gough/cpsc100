b683f2c335453af0319ad51a69e5ed957ed182fa
Adam Gough <a@adamgough.ca>
2014-10-22 14:46:47 -0700

    Added lab7 files

---
+++ lab7/Student.java
@@ -0,0 +1,14 @@
+public class Student {
+	// declare and initialize name variable in Student class
+	private static String name = "";
+
+	public static String getName() {
+		// getName() returns the student's name
+		return Student.name;
+	}
+
+	public static void setName(String s) {
+		// setName() sets the student's name according to the variable passed
+		Student.name = s;
+	}
+}
